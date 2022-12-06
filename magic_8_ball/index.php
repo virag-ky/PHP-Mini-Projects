e1281c92b66735844eacd7faee7cf0e113e150ef
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-06 19:31:23 +0200

    Add magic_8_ball mini-project

--- magic_8_ball/index.php
+++ magic_8_ball/index.php
@@ -9,65 +9,66 @@ function magic8Ball() {
 
   switch($random_num) {
     case 0:
-    print "It is certain.";
+    print "It is certain.\n";
     break;
     case 1:
-    print "It is decidedly so.";
+    print "It is decidedly so.\n";
     break;
     case 2:
-    print "Without a doubt.";
+    print "Without a doubt.\n";
+    break;
     case 3:
-    print "Yes - definitely.";
+    print "Yes - definitely.\n";
     break;
     case 4:
-    print "You may rely on it.";
+    print "You may rely on it.\n";
     break;
     case 5: 
-    print "As I see it, yes.";
+    print "As I see it, yes.\n";
     break;
     case 6:
-    print "Most likely.";
+    print "Most likely.\n";
     break;
     case 7:
-    print "Outlook good.";
+    print "Outlook good.\n";
     break;
     case 8:
-    print "Yes.";
+    print "Yes.\n";
     break;
     case 9:
-    print "Signs point to yes.";
+    print "Signs point to yes.\n";
     break;
     case 10:
-    print "Reply hazy, try again.";
+    print "Reply hazy, try again.\n";
     break;
     case 11:
-    print "Ask again later.";
+    print "Ask again later.\n";
     break;
     case 12:
-    print "Better not tell you now.";
+    print "Better not tell you now.\n";
     break;
     case 13:
-    print "Cannot predict now.";
+    print "Cannot predict now.\n";
     break;
     case 14:
-    print "Concentrate and ask again.";
+    print "Concentrate and ask again.\n";
     break;
     case 15:
-    print "Don't count on it.";
+    print "Don't count on it.\n";
     break;
     case 16:
-    print "My reply is no.";
+    print "My reply is no.\n";
     break;
     case 17:
-    print "My sources say no.";
+    print "My sources say no.\n";
     break;
     case 18:
-    print "Outlook not so good.";
+    print "Outlook not so good.\n";
     break;
     case 19:
-    print "Very doubtful.";
+    print "Very doubtful.\n";
     break;
   }
 };
 
-echo magic8Ball();
+magic8Ball();
